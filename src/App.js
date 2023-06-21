42e66b828b965ee8141e281708777f850edc9849
Fardheen-N <fardheen.n2021csea@sece.ac.in>
2023-06-21 18:42:43 +0530

    state and setState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
-import Welcome from './components/Welcome';
+// import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
+import Counter from './components/Counter';
 function App() {
   return (
     <div className="App">
@@ -14,7 +15,8 @@ function App() {
         <button>Action</button>
       </Greet>
       <Welcome name='sanji' heroName ='BlackLeg '></Welcome> */}
-      <Message></Message>
+      {/* <Message></Message> */}
+      <Counter/>
     </div>
   );
 }
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,8 +1,25 @@
 import React,{Component} from 'react'
 
 class Message extends Component{
+    constructor(){
+        super()
+        this.state = {
+            message : "welcome to the DW",
+            hit : "got you"
+        }
+    }
+    changeMessage(){
+        this.setState({
+            message:'yoo bro you actually hit the button ,your gonna die'
+        })
+    }
     render(){
-        return <h1>Welcome visitor</h1>
+        return (
+            <div>
+                <h1>{this.state.message}</h1>
+                <button onClick={()=> this.changeMessage()}>Hit me</button>
+            </div>
+        )
     }
 }
 
