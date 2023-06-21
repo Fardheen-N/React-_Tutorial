49c74dd35eb4f2233fa160f2790e93c104bbab2b
Fardheen-N <fardheen.n2021csea@sece.ac.in>
2023-06-21 17:31:42 +0530

    nothing

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
+import Message from './components/Message';
 function App() {
   return (
     <div className="App">
-      <Greet name='luffy' heroName ='king of pirates'>
+      {/* <Greet name='luffy' heroName ='king of pirates'>
         <p>This is you pro</p>
       </Greet>
       <Greet name="zoro" heroName = 'king of hell'>
         <button>Action</button>
       </Greet>
-      <Welcome name='sanji' heroName ='BlackLeg '></Welcome>
+      <Welcome name='sanji' heroName ='BlackLeg '></Welcome> */}
+      <Message></Message>
     </div>
   );
 }
